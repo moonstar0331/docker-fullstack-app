bb8205b6ceedff31b9286e32ae388d0dc71929dd
MoonStar <moonsung0331@gmail.com>
2023-10-06 18:01:39 +0900

    Add MySQL

---
+++ mysql/sqls/initialize.sql
@@ -0,0 +1,11 @@
+# 데이터베이스와 테이블을 만들어줍니다.
+DROP TABLE IF EXISTS myapp;
+
+CREATE DATABASE myapp;
+USE myapp;
+
+CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+);
