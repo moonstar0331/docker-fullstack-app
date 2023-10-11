641ba4962f288113639982ec424f6b9dbecaa5be
MoonStar <moonsung0331@gmail.com>
2023-10-11 11:33:57 +0900

    refactor: Update MySQL Dockerfile and Init SQL

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -1,9 +1,14 @@
-GRANT ALL PRIVILEGES on *.* TO 'root'@'%' WITH GRANT OPTION;
+# GRANT ALL PRIVILEGES on *.* TO 'root'@'172.18.0.%' WITH GRANT OPTION;
+# flush privileges;
+
+# GRANT ALL PRIVILEGES on *.* TO 'root'@'%' WITH GRANT OPTION;
 
 # 데이터베이스와 테이블을 만들어줍니다.
-DROP TABLE IF EXISTS myapp;
+
+DROP DATABASE IF EXISTS myapp;
 
 CREATE DATABASE myapp;
+
 USE myapp;
 
 CREATE TABLE lists (
